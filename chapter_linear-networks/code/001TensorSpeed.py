34f219f4ddd59fac2a7b5b774be2bcd62a0b33f3
xkzhai-ser <xkzhai@ser.com>
2024-11-17 02:08:03 +0000

    tensor speed test

--- chapter_linear-networks/code/001TensorSpeed.py
+++ chapter_linear-networks/code/001TensorSpeed.py
@@ -8,6 +8,22 @@ n = 10000
 a = torch.ones([n])
 b = torch.ones([n])
 
+class Timer:
+    def __init__(self):
+        self.times = []
+        self.start()
+    def start(self):
+        self.tik = time.time()
+    def stop(self):
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
 c = torch.zeros(n)
+timer = Timer()
 for i in range(n):
     c[i] = a[i] + b[i]
+print(f'{timer.stop():.5f} sec')
+
+timer.start()
+d = a + b
+print(f'{timer.stop():.5f} sec')
